f84e80d768c69eac16b20c57749166eb9860703f
abdellatif9595 <84909001+abdellatif9595@users.noreply.github.com>
2024-10-17 17:55:19 +0200

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,79 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate Full Stack Developer and Business Intelligence Engineer with a strong passion for developing innovative web solutions, creating analytical dashboards, and leveraging Machine Learning and Artificial Intelligence (AI) technologies. With a degree in Computer Engineering, specializing in Business Intelligence , I have gained solid technical skills in front-end and back-end development, data analysis, and AI-driven solutions. My professional experiences have allowed me to work on various projects, ranging from developing complex web applications to implementing BI and AI solutions that help companies make data-driven decisions. I am also certified in several key technologies, enabling me to bring deep expertise in all aspects of development, data analysis, and AI.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer and BI Engineer with a passion for creating efficient and user-friendly solutions. With a professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "February 2024 - Present",
+    role: "Business intelligence engineer ",
+    company: "BeeCoders",
+    description: `Sales Decision Insights: Business Uderstanding, Data Uderstanding, Data prepation, Modeling
+,Evaluation, Deployment, Design a dashboard that helps thez compagny to make decisions about sales startegies, customer targeting, pricing and product offerings. And build a website that allows the user to log in and register and
+befiniciate the services.`,
+    technologies: ["Python","ETL", "Talend", "PowerBi", "DAX" , "Exel", "PHP" , "Symfony" , "SQL" , "Flask","Machine Learning"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Web Developer Intern",
+    company: "Autonomous Port of Nouadhibou( Mauritania)",
+    description: `Inventory management Web Application : This web application is designed to streamline the process of managing inventory for businesses, allowing users to efficiently track products, manage stock levels, and generate reports. It provides a user-friendly interface and robust backend functionalities to handle various inventory operations such as adding, updating, deleting, and viewing product details.`,
+    technologies: ["HTML", "CSS", "JavaSript", "Reactjs", "Nodejs","SQL", "MySQL"],
+  },
+  {
+    year: "2022 - 2022",
+    role: "Full stack Developer Intern",
+    company: "Tunisian Post Office",
+    description: `Post Office Web Application`,
+    technologies: ["HTML", "CSS", "Bootstrap", "Javasript", "Reactjs", "Nodejs", "SQL"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Association HumanityWithout Borders",
+    description: `Management Association Web application`,
+    technologies: ["HTML", "CSS","Bootstrap","JavaScript","Reactjs","Nodejs","SQL"]
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "PHP", "MySQl", "Symfony","Bootstrap", "Javasript"],
+  },
+  {
+    title: "Inventory management Web Application",
+    image: project2,
+    description: 
+    "This web application is designed to streamline the process of managing inventory for businesses, allowing users to efficiently track products, manage stock levels, and generate reports. It provides a user-friendly interface and robust backend functionalities to handle various inventory operations such as adding, updating, deleting, and viewing product details.",
+    technologies: ["HTML", "CSS", "JavaSript", "Reactjs", "Nodejs","SQL", "MySQL"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS","React", "Tailwind CSS","Next.js","Framer Motion","Vite"],
+  },
+  {
+    title: "Sales decision insights",
+    image: project4,
+    description:
+      "web application containing a decision-making dashboard for financial analysis",
+    technologies: ["Python","ETL", "Talend", "PowerBi", "DAX" , "Exel", "PHP" , "Symfony" , "SQL" , "Flask","Machine Learning"],
+  },
+  
+];
+
+export const CONTACT = {
+  address: "Tunis, Tunisia ",
+  phoneNo: "+222 4777 6444 / +216 5269 6148",
+  email: "abdellatifsow69@gmail.com",
+};
